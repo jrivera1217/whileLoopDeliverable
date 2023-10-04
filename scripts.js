7f52fc1b22c09fb928b4625b528706f25b819bc3
jrivera1217 <144291459+jrivera1217@users.noreply.github.com>
2023-10-04 13:38:12 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function findEven(n){
+    let i = 2;
+    while(i <= n){
+        console.log(i)
+        i += 2
+    }
+}
+console.log("Even numbers up to 10:"); 
+findEven(10); 
+console.log("Even numbers up to 20:"); 
+findEven(20); 
